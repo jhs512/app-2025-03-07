f20b1729fb5f56b99ad8346841e70b84873f5a70
jhs512 <jangka512@gmail.com>
2025-03-07 12:16:02 +0900

    work

---
+++ backend/src/main/kotlin/com/back/domain/post/post/controller/ApiV1PostController.kt
@@ -0,0 +1,18 @@
+package com.back.domain.post.post.controller
+
+import com.back.domain.post.post.entity.Post
+import com.back.domain.post.post.service.PostService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/posts")
+class ApiV1PostController(
+    private val postService: PostService
+) {
+    @GetMapping
+    fun getItems(): List<Post> {
+        return postService.findAll()
+    }
+}
